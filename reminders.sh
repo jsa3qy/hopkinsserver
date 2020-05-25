a4444bbe4eb28ca6ba0d330b3770d3a35b059d9c
Hopkins House <jesse@ubuntu.lan>
2020-05-25 14:09:33 +0000

    absolute path added to ssmtp to stop cron job failure

--- reminders.sh
+++ reminders.sh
@@ -3,4 +3,4 @@
 EMAILS="jessealloy@gmail.com,connorhagan20@gmail.com,adisrikanth.aks@gmail.com,will17.r@gmail.com"
 pip3 install -r requirements.txt >/dev/null
 python3 main.py $EMAILS > msg.txt
-ssmtp $EMAILS < msg.txt
+/usr/sbin/ssmtp $EMAILS < msg.txt
